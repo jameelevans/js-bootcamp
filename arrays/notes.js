f6c41e175c57ba4a62a4b5e1f366f009f45df965
Jameel <jameelevans@live.com>
2019-08-26 12:08:14 -0500

    Manipulating Arrays with Methods part 2

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,10 +1,35 @@
 const notes = ['Note 1', 'Note 2', 'Note 3']
 
+// //Removes an item from the end of array
+// console.log(notes.pop())
+
+// // Add new item on to the end of array
+// notes.push('My new note')
+
+// //Removes very first item from array
+// console.log(notes.shift())
+
+// // Adds a note to the very begining
+// notes.unshift('My first note')
+
+//Starts at 1 positons then delete 1 item
+notes.splice(1, 1)
+
+//Add a new item in second spot and moves all other items over 1 spot
+//notes.splice(1, 0, 'This is the new second item')
+
+//Add a new item in second spot replaces the item in place
+//notes.splice(1, 1, 'This is the new second item')
+
+notes[2] = 'This is now the new note 3'
+
 //Get the number of items inside an array
 console.log(notes.length)
 
 //Get the last item of an array 
-console.log(notes[notes.length - 1])
+// console.log(notes[notes.length - 1])
 
 //Get the second to last item in an array
-console.log(notes[notes.length - 2])
+// console.log(notes[notes.length - 2])
+console.log(notes)
+
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,9 +1,22 @@
 //Create an array with 5 things to do
 const todos = ['Walk the dog', 'Feed the fish', 'Change kitty litter', 'Buy groceries', 'Wash the car']
 
+ // *ARRAYS Manipulating Arrays with Methods Cahallenge*
+
+//Delete the thrid item
+todos.splice(2, 1)
+
+//Add new item to the end
+todos.push('My new item on end')
+
+//Delete the first item from list
+todos.shift()
+
 //Print message that include length. You have x todos
 console.log(`You have ${todos.length} items on your to do list!`)
+console.log(todos)
 
 //Print the first and second to last items to terminal -> Todo: Walk the Dog
-console.log(`Todo: ${todos[0]}`)
-console.log(`Todo: ${todos[todos.length - 2]}`)
+//console.log(`Todo: ${todos[0]}`)
+//console.log(`Todo: ${todos[todos.length - 2]}`)
+
