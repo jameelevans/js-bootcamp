0713e41b59338d20fa5d0bc73c20b230c9138e06
Jameel <jameelevans@live.com>
2019-08-26 11:40:05 -0500

    Manipulating Arrays with Methods

---
+++ arrays/notes.js
@@ -0,0 +1,10 @@
+const notes = ['Note 1', 'Note 2', 'Note 3']
+
+//Get the number of items inside an array
+console.log(notes.length)
+
+//Get the last item of an array 
+console.log(notes[notes.length - 1])
+
+//Get the second to last item in an array
+console.log(notes[notes.length - 2])
---
+++ arrays/todo.js
@@ -0,0 +1,9 @@
+//Create an array with 5 things to do
+const todos = ['Walk the dog', 'Feed the fish', 'Change kitty litter', 'Buy groceries', 'Wash the car']
+
+//Print message that include length. You have x todos
+console.log(`You have ${todos.length} items on your to do list!`)
+
+//Print the first and second to last items to terminal -> Todo: Walk the Dog
+console.log(`Todo: ${todos[0]}`)
+console.log(`Todo: ${todos[todos.length - 2]}`)
